da989980832a4548fc15c02c41a93e767904d951
Murilo Alves <122987388+muriloalvestravelpass@users.noreply.github.com>
2026-04-23 09:49:58 -0300

    Enhance DATE.sql with additional queries to calculate age from data_nascimento and filter students born in February.

--- sql/select/date/DATE.sql
+++ sql/select/date/DATE.sql
@@ -5,3 +5,11 @@ SELECT DATE('now', '-10 days'); /*2026-04-12*/
 SELECT julianday('now') - julianday('2023-04-01');
 
 SELECT julianday('2023-04-10') - julianday('2023-04-01'); /*9*/
+
+SELECT * FROM Alunos WHERE STRFTIME('%m', data_nascimento) = '02'; /*2026-02-01*/
+
+SELECT nome_aluno,
+      data_nascimento,
+      (strftime('%Y', CURRENT_DATE) - strftime('%Y', data_nascimento)) - 
+      (strftime('%m-%d', CURRENT_DATE) < strftime('%m-%d', data_nascimento)) AS idade
+FROM Alunos;

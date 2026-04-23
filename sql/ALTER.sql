5a73fb6e691f87bed1face49308b6bb71ad1c576
Murilo Alves <122987388+muriloalvestravelpass@users.noreply.github.com>
2026-04-23 09:23:11 -0300

    Update ALTER.sql: add data_nascimento column to funcionarios table while maintaining previous changes to ENDERECO_CLIENTE, Idade, and HistoricoEmprego.

--- sql/ALTER.sql
+++ sql/ALTER.sql
@@ -3,3 +3,5 @@ ALTER table tabelaclientes ADD ENDERECO_CLIENTE VARCHAR (250);
 ALTER TABLE Estudantes DROP COLUMN Idade;
 
 ALTER TABLE HistoricoEmprego RENAME TO CargosColaboradores;
+
+ALTER TABLE funcionarios RENAME COLUMN data_nasc TO data_nascimento;
